19531442e7885ab6e10cde363641257ad5dd57ff
Rafael Henrique <rafa.zqn@gmail.com>
2026-04-29 13:44:49 -0300

    Delete escapefromthehouse directory

--- escapefromthehouse/rooms.js
+++
@@ -1,7 +0,0 @@
-export const rooms = [
-    { id: "bedroom", connections: ["livingroom"] },
-    { id: "livingroom", connections: ["bedroom", "kitchen"] },
-    { id: "kitchen", connections: ["livingroom", "garage"] },
-    { id: "garage", connections: ["kitchen", "backyard"] },
-    { id: "backyard", connections: ["garage"] }
-];
